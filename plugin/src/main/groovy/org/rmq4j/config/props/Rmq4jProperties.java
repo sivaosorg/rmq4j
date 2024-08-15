0df123f95e56f57c0dca27c219a4dbc5a2544f80
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-08-15 20:54:19 +0700

    :sparkles: feat: add base properties config #4

--- plugin/src/main/groovy/org/rmq4j/config/props/Rmq4jProperties.java
+++ plugin/src/main/groovy/org/rmq4j/config/props/Rmq4jProperties.java
@@ -4,11 +4,128 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.Map;
 
 @Component
 @ConfigurationProperties(prefix = "spring.rmq4j")
 public class Rmq4jProperties implements Serializable {
     public Rmq4jProperties() {
         super();
+    }
+
+    private boolean enabled = false;
+    private boolean debugging = false;
+    private String defaultCluster; // default_cluster
+    private Map<String, Node> clusters;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isDebugging() {
+        return debugging;
+    }
+
+    public void setDebugging(boolean debugging) {
+        this.debugging = debugging;
+    }
+
+    public String getDefaultCluster() {
+        return defaultCluster;
+    }
+
+    public void setDefaultCluster(String defaultCluster) {
+        this.defaultCluster = defaultCluster;
+    }
+
+    public Map<String, Node> getClusters() {
+        return clusters;
+    }
+
+    public void setClusters(Map<String, Node> clusters) {
+        this.clusters = clusters;
+    }
+
+    public static class Node {
+        public Node() {
+            super();
+        }
+
+        private boolean enabled = false;
+        private String host;
+        private int port;
+        private String username;
+        private String password;
+        private String virtualHost; // virtual_host
+        private boolean useSSL = false; // use_ssl
+        private String desc;
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getVirtualHost() {
+            return virtualHost;
+        }
+
+        public void setVirtualHost(String virtualHost) {
+            this.virtualHost = virtualHost;
+        }
+
+        public boolean isUseSSL() {
+            return useSSL;
+        }
+
+        public void setUseSSL(boolean useSSL) {
+            this.useSSL = useSSL;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
     }
 }
--- plugin/src/main/groovy/org/rmq4j/service/impl/Rmq4jServiceImpl.java
+++ plugin/src/main/groovy/org/rmq4j/service/impl/Rmq4jServiceImpl.java
@@ -1,12 +1,21 @@
 package org.rmq4j.service.impl;
 
+import org.rmq4j.config.props.Rmq4jProperties;
+import org.rmq4j.service.Rmq4jService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.rmq4j.service.Rmq4jService;
 
 @SuppressWarnings({"FieldCanBeLocal", "DuplicatedCode"})
 @Service
 public class Rmq4jServiceImpl implements Rmq4jService {
     protected static final Logger logger = LoggerFactory.getLogger(Rmq4jServiceImpl.class);
+
+    protected final Rmq4jProperties properties;
+
+    @Autowired
+    public Rmq4jServiceImpl(Rmq4jProperties properties) {
+        this.properties = properties;
+    }
 }
