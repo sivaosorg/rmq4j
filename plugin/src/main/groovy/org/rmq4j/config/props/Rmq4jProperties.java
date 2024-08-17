0b2f00ae3f7ced4e7bd409ac09dc425f9b02ba7d
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-08-17 22:56:28 +0700

    :wrench: chore: update rmq4j properties #4

--- plugin/src/main/groovy/org/rmq4j/config/props/Rmq4jProperties.java
+++ plugin/src/main/groovy/org/rmq4j/config/props/Rmq4jProperties.java
@@ -4,7 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -18,6 +20,7 @@ public class Rmq4jProperties implements Serializable {
     private boolean debugging = false;
     private String defaultCluster; // default_cluster
     private Map<String, Connection> clusters = new HashMap<>(); // clusters
+    private List<Config> configs = new ArrayList<>(); // configs
 
     public boolean isEnabled() {
         return enabled;
@@ -49,6 +52,14 @@ public class Rmq4jProperties implements Serializable {
 
     public void setClusters(Map<String, Connection> clusters) {
         this.clusters = clusters;
+    }
+
+    public List<Config> getConfigs() {
+        return configs;
+    }
+
+    public void setConfigs(List<Config> configs) {
+        this.configs = configs;
     }
 
     public static class Connection {
@@ -127,6 +138,216 @@ public class Rmq4jProperties implements Serializable {
 
         public void setDesc(String desc) {
             this.desc = desc;
+        }
+    }
+
+    public static class Config {
+        public Config() {
+            super();
+        }
+
+        private String clusterKey; // cluster_key
+        private String dispatchKey; // dispatch_key
+        private String desc;
+        private boolean enabled = false;
+        private boolean debugging = false;
+        private Exchange exchange = new Exchange(); // exchange
+        private Queue queue = new Queue(); // queue
+        private Bind bind = new Bind(); // bind
+
+        public String getClusterKey() {
+            return clusterKey;
+        }
+
+        public void setClusterKey(String clusterKey) {
+            this.clusterKey = clusterKey;
+        }
+
+        public String getDispatchKey() {
+            return dispatchKey;
+        }
+
+        public void setDispatchKey(String dispatchKey) {
+            this.dispatchKey = dispatchKey;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public boolean isDebugging() {
+            return debugging;
+        }
+
+        public void setDebugging(boolean debugging) {
+            this.debugging = debugging;
+        }
+
+        public Exchange getExchange() {
+            return exchange;
+        }
+
+        public void setExchange(Exchange exchange) {
+            this.exchange = exchange;
+        }
+
+        public Queue getQueue() {
+            return queue;
+        }
+
+        public void setQueue(Queue queue) {
+            this.queue = queue;
+        }
+
+        public Bind getBind() {
+            return bind;
+        }
+
+        public void setBind(Bind bind) {
+            this.bind = bind;
+        }
+    }
+
+    @SuppressWarnings({"SpellCheckingInspection"})
+    public static class Exchange {
+        public Exchange() {
+            super();
+        }
+
+        private String name;
+        private String kind;
+        private boolean durable = false;
+        private boolean clearable = false;
+        private boolean internal = false;
+        private boolean delayable = false;
+        private Map<String, Object> arguments = new HashMap<>(); // arguments
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getKind() {
+            return kind;
+        }
+
+        public void setKind(String kind) {
+            this.kind = kind;
+        }
+
+        public boolean isDurable() {
+            return durable;
+        }
+
+        public void setDurable(boolean durable) {
+            this.durable = durable;
+        }
+
+        public boolean isClearable() {
+            return clearable;
+        }
+
+        public void setClearable(boolean clearable) {
+            this.clearable = clearable;
+        }
+
+        public boolean isInternal() {
+            return internal;
+        }
+
+        public void setInternal(boolean internal) {
+            this.internal = internal;
+        }
+
+        public boolean isDelayable() {
+            return delayable;
+        }
+
+        public void setDelayable(boolean delayable) {
+            this.delayable = delayable;
+        }
+
+        public Map<String, Object> getArguments() {
+            return arguments;
+        }
+
+        public void setArguments(Map<String, Object> arguments) {
+            this.arguments = arguments;
+        }
+    }
+
+    @SuppressWarnings({"SpellCheckingInspection"})
+    public static class Queue {
+        public Queue() {
+            super();
+        }
+
+        private String name;
+        private boolean durable = false;
+        private boolean exclusive = false;
+        private boolean clearable = false;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public boolean isDurable() {
+            return durable;
+        }
+
+        public void setDurable(boolean durable) {
+            this.durable = durable;
+        }
+
+        public boolean isExclusive() {
+            return exclusive;
+        }
+
+        public void setExclusive(boolean exclusive) {
+            this.exclusive = exclusive;
+        }
+
+        public boolean isClearable() {
+            return clearable;
+        }
+
+        public void setClearable(boolean clearable) {
+            this.clearable = clearable;
+        }
+    }
+
+    public static class Bind {
+        public Bind() {
+            super();
+        }
+
+        private String routingKey; // routing_key
+
+        public String getRoutingKey() {
+            return routingKey;
+        }
+
+        public void setRoutingKey(String routingKey) {
+            this.routingKey = routingKey;
         }
     }
 }
