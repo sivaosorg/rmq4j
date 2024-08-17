4f3d219e24ecbbfd56654890cbeb7203e20ffb79
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-08-17 11:20:48 +0700

    :recycle: refactor: refactor codebase #4

--- plugin/src/main/groovy/org/rmq4j/config/Rmq4jConfig.java
+++ plugin/src/main/groovy/org/rmq4j/config/Rmq4jConfig.java
@@ -1,7 +1,21 @@
 package org.rmq4j.config;
 
+import org.rmq4j.service.Rmq4jInsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Rmq4jConfig {
+    protected final Rmq4jInsService rmq4jInsService;
+
+    @Autowired
+    public Rmq4jConfig(Rmq4jInsService rmq4jInsService) {
+        this.rmq4jInsService = rmq4jInsService;
+    }
+
+    @Bean
+    public void snapIns() {
+        rmq4jInsService.snapIns();
+    }
 }
